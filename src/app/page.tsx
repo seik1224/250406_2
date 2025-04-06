8400b1c33783aef97c86570902946ea24c0ae7d1
SBS세익 <seik1224@naver.com>
2025-04-06 11:39:17 +0900

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 import Step01 from "@/step/step01";
+import Step02 from "@/step/step02";
 
 export default function Home() {
   return (
    <>
-    <Step01/>
+    {/* <Step01/> */}
+    <Step02 />
    </>
   );
 }
---
+++ src/step/step02.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Step02 = () => {
+    return (
+        <div>Step02</div>
+    )
+}
+
+export default Step02
